8d928e794a89d575d0a6ead269621edf0fb8d283
Yasmeen Noor De Villiers <yasmeennoordv@localhost.localdomain>
2023-03-03 00:20:49 +0200

    New PythonAnywhere webhook attempt.

--- meque.py
+++ meque.py
@@ -3,8 +3,9 @@
 # Date: 2 March 2023
 
 #Imports
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 #import flask_login
+import git
 
 #Initialize app
 app = Flask(__name__)
@@ -18,6 +19,18 @@ app = Flask(__name__)
 @app.route("/")
 def home():
 	return render_template('home.html')
+
+#Monitor a webhook to allow PythonAnywhere to automatically deploy new commits
+@app.route('/webhook', methods=['POST'])
+def webhook():
+	if request.method == 'POST':
+		repo = git.Repo.('./meque')
+		meque = repo.remotes.meque
+		repo.create_head('main',  meque.refs.main).set_tracking_branch(meque.refs.main).checkout()
+		meque.pull()
+		return '', 200
+	else:
+		return '', 400
 
 #Basic python app run
 if __name__ == '__main__':
