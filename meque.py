3a2a7bf595204eb24c9ab7c723ad779d6bb40b0a
Yasmeen Noor De Villiers <yasmeennoordv@localhost.localdomain>
2023-03-02 20:21:13 +0200

    Created a basic HTML home page and began preparing for user session logins

--- meque.py
+++ meque.py
@@ -3,12 +3,22 @@
 # Date: 2 March 2023
 
 #Imports
-from flask import Flask
+from flask import Flask, render_template
+#import flask_login
 
 #Initialize app
-app=Flask(__name__)
+app = Flask(__name__)
+#app.secret_key = 'science rift under'
+
+#User session setup
+#login_manager = flask_login.LoginManager()
+#login_manager.init_app(app)
 
 #Home page
 @app.route("/")
 def home():
-	return "<h1>Meque</h1>"
+	return render_template('home.html')
+
+#Basic python app run
+if __name__ == '__main__':
+	app.run()
